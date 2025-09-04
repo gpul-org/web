3d1002e4193f526639f8ebb3d372fd73b9f38e46
GamuWasTaken <victordiezruiz@proton.me>
2025-09-04 21:12:36 +0200

    News collection done and integrated + better filters for collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 
-const events = defineCollection ({
+const events = defineCollection({
   loader: glob({ pattern: '*.md', base: './src/content/events'}),
   schema: z.object({
     title: z.string(),
@@ -14,5 +14,26 @@ const events = defineCollection ({
   })
 });
 
+export const isUpcomingEvent = ({ data }) => data.status === "upcoming" 
+export const isPastEvent = event => !isUpcomingEvent(event) 
 
-export const collections = { events };
+const news = defineCollection({
+  loader: glob({ pattern: '*.md', base: './src/content/news'}),
+  schema : z.object({
+    title: z.string(),
+    excerpt: z.string(),
+    date: z.date(),
+    author: z.string(),
+    tags: z.array(z.string()),
+  })
+});
+
+export const isRecentNew = ({ data }) => {
+  const recentTime = new Date();
+  recentTime.setFullYear(recentTime.getFullYear() - 1);
+
+  return data.date < recentTime;
+} 
+
+
+export const collections = { events, news };
---
+++ src/content/news/26-aniversario.md
@@ -0,0 +1,8 @@
+---
+title: GPUL celebra 26 anos promovendo a cultura libre
+excerpt: Un ano máis, GPUL continúa coa súa misión de defender e promover o software libre, o hardware aberto e o coñecemento libre na comunidade universitaria.
+date: 2024-01-10
+author: Redacción
+href: /news/26-anos-gpul
+tags: [Aniversario, Historia]
+---
