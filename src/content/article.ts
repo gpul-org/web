cc6647b3ddd3935cf67078363df6835907473e48
GamuWasTaken <victordiezruiz@proton.me>
2025-09-11 16:09:15 +0200

    CTA, ContentDisplay, ContentCards and Mission

--- src/content/article.ts
+++ src/content/article.ts
@@ -3,4 +3,6 @@ import { getCollection, type CollectionEntry } from 'astro:content';
 
 export type Article = CollectionEntry<'news'>;
 
-export const articles = await getCollection('news');
+export const articles = (await getCollection('news')).toSorted(
+  (a, b) => b.data.date.getTime() - a.data.date.getTime()
+);
--- src/content/directive.ts
+++ src/content/directive.ts
@@ -1,3 +1,5 @@
-import { getCollection } from "astro:content"
+import { getCollection, type CollectionEntry } from "astro:content"
 
+
+export type Directive = CollectionEntry<'directive'>;
 export const directive = await getCollection('directive')
