a3e36892c0c396d60d623cf580ab0407c11a310a
Pedro Rey Anca <personal@peprolinbot.com>
2025-10-18 17:43:28 +0200

    Support multiple authors with portfolios

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { glob } from 'astro/loaders';
 import { defineCollection } from 'astro:content';
 
-import { definition as eventos } from './event';
+import { definition as eventos, authors_definition as authors } from './event';
 import { definition as novas} from './nova'; 
 import { definition as directiva } from './directive'; 
 
@@ -10,4 +10,4 @@ import { definition as directiva } from './directive';
 // });
 
 
-export const collections = { eventos, novas, directiva, /*history*/ };
+export const collections = { eventos, novas, directiva, authors, /*history*/ };
--- src/content/event.ts
+++ src/content/event.ts
@@ -1,7 +1,8 @@
-import { glob } from "astro/loaders";
+import { glob, file } from "astro/loaders";
 import {
   type CollectionEntry,
   defineCollection,
+  reference,
   getCollection,
   z,
 } from "astro:content";
@@ -15,8 +16,16 @@ export const definition = defineCollection({
     location: z.string(),
     tags: z.array(z.string()),
     status: z.string(),
-    author: z.string().optional(),
+    authors: z.array(reference('authors')).optional(),
   }),
+});
+
+export const authors_definition = defineCollection({
+  loader: file('./src/content/authors.json'),
+  schema: z.object({
+    id: z.string(),
+    portfolio: z.string().url(),
+  })
 });
 
 export type Event = CollectionEntry<"eventos">;
